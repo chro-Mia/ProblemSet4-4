eb5e716898d1a28c2add40730bf3ff70bd2b2171
chro-Mia <tessa.chromia@gmail.com>
2025-01-20 22:02:34 -0800

    rot

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,82 @@
 public class Main {
     public static void main(String[] args){
-        int[] array = new int[]{6,4,3,2,6};
-        selectionSort(array);
-        for(int ints : array){
-            System.out.println(ints);
+        int[] array = new int[1000];
+        for(int i = 0; i < 1000; i++){
+            int a = (int)(Math.random() * 100);
+            array[i] = a;
         }
+        insertionSort(array);
+
+        long startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++){
+            int a = (int)(Math.random() * 100);
+            binarySearch(array, a);
+        }
+        long duration = System.nanoTime() - startTime;
+        System.out.println(duration);
     }
+    //binary search is much faster especially for larger arrays
+    //linear search gets cooked by large arrays
 
     public static int[] selectionSort(int[] array){
-        int lowest = array[0];
         int index = 0;
+
         for(int i = 0; i < array.length; i++){
-            if(array[i] < lowest){
-                lowest = array[i];
-                index = i;
+            int lowest = array[i];
+            for(int j = i; j < array.length; j++){
+                if(array[j] <= lowest){
+                    lowest = array[j];
+                    index = j;
+                }
+            }
+            array[index] = array[i];
+            array[i] = lowest;
+        }
+        return array;
+    }
+
+    public static int[] insertionSort(int[] array){
+        for(int i = 1; i < array.length; i++){
+            int intToSort = array[i];
+            int j = i - 1;
+
+            while(j >= 0 && array[j] > intToSort){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = intToSort;
+        }
+
+
+        return array;
+    }
+
+    public static int binarySearch(int[] array, int numToFind){
+        int min = 0;
+        int max = array.length - 1;
+
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+            if (array[mid] == numToFind){
+                return mid;
+            }
+            if (array[mid] < numToFind){
+                min = mid + 1;
+            }
+            else{
+                max = mid - 1;
             }
         }
-        array[index] = array[0];
-        array[0] = lowest;
-        return array;
+
+        return -1;
+    }
+
+    public static int linearSearch(int[] array, int numToFind){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == numToFind){
+                return i;
+            }
+        }
+        return -1;
     }
 }
